b5891553d3ed8ab0c822b72d6e0e95e1fafe6294
Nikita <nikita119@protonmail.com>
2024-06-30 17:59:35 +0300

    error validation, loader

---
+++ frontend/src/pages/EventsRoot.js
@@ -0,0 +1,11 @@
+import EventsNavigation from "../components/EventsNavigation";
+import {Outlet} from "react-router-dom";
+
+export default function EventsRootPage() {
+    return (
+        <>
+            <EventsNavigation />
+            <Outlet />
+        </>
+    );
+}
---
+++ frontend/src/pages/Root.js
@@ -0,0 +1,16 @@
+import MainNavigation from "../components/MainNavigation";
+import {Outlet, useLoaderData, useNavigation} from "react-router-dom";
+
+export default function RootLayout() {
+
+    // const navigation = useNavigation();
+
+
+    return <>
+        <MainNavigation />
+        <main>
+            {/*{navigation.state === 'loading' && <p>Loading...</p>}*/}
+            <Outlet />
+        </main>
+    </>
+}
